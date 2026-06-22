417eb621f5a1d827ffc77da890ab4a6e3de0cf6b
SKYLINE <skyline@gmail.com>
2026-06-22 18:13:09 +0300

    update

--- app.py
+++ app.py
@@ -1581,4 +1581,8 @@ def edit_setting(id):
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
